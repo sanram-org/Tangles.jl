a630ba76dddcf384c1d07677e9f6d5d48f6910e5
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-05-15 09:02:17 +0200

    Reexport TenetCore

--- src/Tangles.jl
+++ src/Tangles.jl
@@ -3,6 +3,9 @@ module Tangles
 using Networks
 import Networks: ImplementorTrait, DelegatorTrait
 
+using Reexport
+@reexport using TenetCore
+
 include("ProductState.jl")
 export ProductState, ProductOperator
 
