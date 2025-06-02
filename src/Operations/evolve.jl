c5f3082c49887597c7c20ece3402a27d32a52a65
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-06-02 17:53:43 +0200

    Implement `evolve!` for `MPS`, `MPO` evolution

--- src/Operations/evolve.jl
+++ src/Operations/evolve.jl
@@ -12,3 +12,34 @@ function evolve! end
 evolve!(tn, op; kwargs...) = evolve!(tn, op, DelegatorTrait(Evolvable(), tn); kwargs...)
 evolve!(tn, op, ::DelegateToField; kwargs...) = evolve!(delegator(Evolvable(), tn), op; kwargs...)
 evolve!(tn, op, ::DontDelegate; algorithm=UnknownAlgorithm(), kwargs...) = evolve!(algorithm, tn, op; kwargs...)
+
+# TODO use an `Algorithm` trait to dispatch on the algorithm
+function evolve!(mps::MPS, op::MPO; kwargs...)
+    @argcheck nsites(mps) == nsites(op) "MPS and MPO must have the same number of sites"
+
+    # align MPS and MPO
+    op = resetinds!(copy(op))
+    align!(mps, :outputs, op, :inputs)
+
+    # TODO do we need @unsafe_region here?
+    for i in 1:nsites(mps)
+        tensor_mps = tensor_at(mps, site"i")
+        tensor_op = tensor_at(op, site"i")
+        c = binary_einsum(tensor_mps, tensor_op)
+        c = replace(c, ind_at(op, plug"i") => ind_at(mps, plug"i"))
+
+        # fuse virtual indices
+        if i > 1
+            j = i - 1
+            c = Muscle.fuse(c, [ind_at(mps, bond"j-i"), ind_at(op, bond"j-i")]; ind=ind_at(mps, bond"j-i"))
+        end
+        if i < nsites(mps)
+            j = i + 1
+            c = Muscle.fuse(c, [ind_at(mps, bond"i-j"), ind_at(op, bond"i-j")]; ind=ind_at(mps, bond"i-j"))
+        end
+
+        replace_tensor!(mps, tensor_mps, c)
+    end
+
+    return mps
+end
