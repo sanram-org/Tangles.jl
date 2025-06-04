5e24438d8f45b63f53e265c5cc51e4a2bd7c5e88
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2025-06-04 15:13:16 +0200

    Implement `generic_evolve_mps_mpo_zipup!` method

--- src/Operations/evolve.jl
+++ src/Operations/evolve.jl
@@ -6,6 +6,8 @@ struct Evolvable <: Interface end
 abstract type EvolveAlgorithm end
 struct UnknownAlgorithm <: EvolveAlgorithm end
 struct SimpleUpdate <: EvolveAlgorithm end
+struct DirectMPOContraction <: EvolveAlgorithm end
+struct ZipUpAlgorithm <: EvolveAlgorithm end
 
 function evolve! end
 
@@ -38,6 +40,94 @@ function generic_evolve_mps_mpo_direct!(mps, op)
         end
 
         replace_tensor!(mps, tensor_mps, c)
+    end
+
+    return mps
+end
+
+# NOTE last bonds use full maxdim size, instead of the one due to boundary effects
+function generic_evolve_mps_mpo_zipup!(mps, op; maxdim=nothing, threhold=nothing)
+    @argcheck nsites(mps) == nsites(op) "MPS and MPO must have the same number of sites"
+    @argcheck !isnothing(maxdim) || !isnothing(threhold) "Either `maxdim` or `threhold` must be specified, or use"
+
+    # align MPS and MPO
+    op = resetinds!(copy(op))
+    align!(mps, :outputs, op, :inputs)
+
+    C₁ = binary_einsum(tensor_at(mps, site"1"), tensor_at(op, site"1"))
+    C₁ = replace(C₁, ind_at(op, plug"1") => ind_at(mps, plug"1"))
+
+    # better use truncated `eigen` or truncated `svd`?
+    ind_s = Index(gensym(:tmp))
+    U, S, V = tensor_svd_thin(C₁; inds_u=[ind_at(mps, plug"1")], ind_s)
+    if !isnothing(maxdim)
+        U = view(U, ind_s => 1:min(maxdim, length(S)))
+        S = view(S, ind_s => 1:min(maxdim, length(S)))
+        V = view(V, ind_s => 1:min(maxdim, length(S)))
+    end
+    if !isnothing(threhold)
+        keep = findall(x -> abs(x) > threhold, S)
+        U = view(U, ind_s => keep)
+        S = view(S, ind_s => keep)
+        V = view(V, ind_s => keep)
+    end
+
+    # absorb the singular values into V to shift right the orthogonality center
+    R = binary_einsum(V, S; dims=Index[])
+
+    # rename temporal index to the bond index
+    U = replace(U, ind_s => ind_at(mps, bond"1-2"))
+
+    @unsafe_region mps begin
+        replace_tensor!(mps, tensor_at(mps, site"1"), U)
+
+        for i in 2:(nsites(mps) - 1)
+            _site = site"i"
+            R = binary_einsum(binary_einsum(R, tensor_at(mps, _site)), tensor_at(op, _site))
+            R = replace(R, ind_at(op, plug"i") => ind_at(mps, plug"i"))
+
+            # rename left temporal index to the bond index
+            R = replace(R, ind_s => ind_at(mps, Bond(CartesianSite(i - 1), CartesianSite(i))))
+
+            U, S, V = tensor_svd_thin(
+                R; inds_u=[ind_at(mps, plug"i"), ind_at(mps, Bond(CartesianSite(i - 1), CartesianSite(i)))], ind_s
+            )
+
+            if !isnothing(maxdim)
+                U = view(U, ind_s => 1:min(maxdim, length(S)))
+                S = view(S, ind_s => 1:min(maxdim, length(S)))
+                V = view(V, ind_s => 1:min(maxdim, length(S)))
+            end
+
+            if !isnothing(threhold)
+                keep = findall(x -> abs(x) > threhold, S)
+                U = view(U, ind_s => keep)
+                S = view(S, ind_s => keep)
+                V = view(V, ind_s => keep)
+            end
+
+            # absorb the singular values into V to shift right the orthogonality center
+            R = binary_einsum(V, S; dims=Index[])
+
+            # rename temporal index to the bond index
+            U = replace(U, ind_s => ind_at(mps, Bond(CartesianSite(i), CartesianSite(i + 1))))
+
+            replace_tensor!(mps, tensor_at(mps, _site), U)
+        end
+
+        # last site
+        i = nsites(mps)
+        @show size(R) inds(R) size(tensor_at(mps, site"i")) inds(tensor_at(mps, site"i")) size(tensor_at(op, site"i")) inds(
+            tensor_at(op, site"i")
+        )
+        R = binary_einsum(R, tensor_at(mps, site"i"))
+        R = binary_einsum(R, tensor_at(op, site"i"))
+        R = replace(R, ind_at(op, plug"i") => ind_at(mps, plug"i"))
+
+        # rename left temporal index to the bond index
+        R = replace(R, ind_s => ind_at(mps, Bond(CartesianSite(i - 1), CartesianSite(i))))
+
+        replace_tensor!(mps, tensor_at(mps, site"i"), R)
     end
 
     return mps
