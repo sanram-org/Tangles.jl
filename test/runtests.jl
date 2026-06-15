8b29cbc02695f8228af0f7fb8ed1ccb3215307d5
Sergio Sanchez Ramirez <15837247+mofeing@users.noreply.github.com>
2026-06-15 20:14:21 +0200

    update CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,22 @@
 using Test
 using SafeTestsets
 
-@testset "Unit" verbose = true begin
-    @safetestset "TensorNetwork" include("unit/tensor_network.jl")
-    @safetestset "Taggable" include("unit/taggable.jl")
-    @safetestset "Pluggable" include("unit/pluggable.jl")
-    @safetestset "GenericLattice" include("unit/generic_lattice.jl")
-    @safetestset "LayeredTensorNetwork" include("unit/layered_tensor_network.jl")
+target_testsets = isempty(ARGS) ? ["core", "integration"] : ARGS
+
+if "core" in target_testsets
+    @testset "Unit" verbose = true begin
+        @safetestset "TensorNetwork" include("unit/tensor_network.jl")
+        @safetestset "Taggable" include("unit/taggable.jl")
+        @safetestset "Pluggable" include("unit/pluggable.jl")
+        @safetestset "GenericLattice" include("unit/generic_lattice.jl")
+        @safetestset "LayeredTensorNetwork" include("unit/layered_tensor_network.jl")
+    end
 end
 
-@testset "Integration" verbose = true begin
-    if !isnothing(get(ENV, "TANGLES_TEST_REACTANT", nothing))
-        @safetestset "Reactant" include("integration/reactant.jl")
+if "integration" in target_testsets
+    @testset "Integration" verbose = true begin
+        if !isnothing(get(ENV, "TANGLES_TEST_REACTANT", nothing))
+            @safetestset "Reactant" include("integration/reactant.jl")
+        end
     end
 end
